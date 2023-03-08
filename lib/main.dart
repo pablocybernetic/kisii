b36f59faefcafe335e27b81470433487af34c033
pablocybernetic <gitaup08@gmail.com>
2023-03-08 17:21:42 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,65 +2,41 @@ import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:kisii/screens/homePage.dart';
 import 'package:kisii/screens/login.dart';
 
-void main() => runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  bool _isLoggedIn = false;
-
-  @override
-  void initState() {
-    super.initState();
-    checkLoginStatus();
-  }
-
-  void checkLoginStatus() async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        print('User is currently signed out!');
-      } else {
-        print('User is signed in!');
-      }
-    });
-
-    var headers = {'Cookie': 'PHPSESSID=nf08rjdfadbg5mklfquvub3ldc; path=/'};
-    var request = http.Request('GET',
-        Uri.parse('https://kisiiuniversity.000webhostapp.com/check_login.php'));
-
-    request.headers.addAll(headers);
-
-    http.StreamedResponse response = await request.send();
-
-    if (response.statusCode == 200) {
-      var responseBody = await utf8.decodeStream(response.stream);
-      var decodedResponse = json.decode(responseBody);
-      if (decodedResponse['login_status'] == true) {
-        print(decodedResponse['login_status']);
-        _isLoggedIn = true;
-      } else if (decodedResponse['login_status'] == false) {
-        _isLoggedIn = false;
-      } else {
-        print('Unknown login status.');
-      }
-    } else {
-      print('Error: ${response.reasonPhrase}');
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      home: Text('hi'),
-      // home: _isLoggedIn ? MyHome() : LoginPage(),
+      title: 'Flutter Authentication',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: _handleAuth(),
+    );
+  }
+
+  Widget _handleAuth() {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          return MyHome();
+        } else {
+          return LoginPage();
+        }
+      },
     );
   }
 }
